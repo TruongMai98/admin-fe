d46a329fb5af64afb5ed33f8a4454b2db229b032
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2023-03-14 08:23:22 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { apiUser } from "state/apiUser";
+import { Provider } from "react-redux";
+import { apiCustomer } from "state/apiCustomer";
+import { apiOrder } from "state/apiOrder";
+import { apiProduct } from "state/apiProduct";
+import { apiCategory } from "state/apiCategory";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+    [apiUser.reducerPath]: apiUser.reducer,
+    [apiOrder.reducerPath]: apiOrder.reducer,
+    [apiCustomer.reducerPath]: apiCustomer.reducer,
+    [apiProduct.reducerPath] : apiProduct.reducer,
+    [apiCategory.reducerPath] : apiCategory.reducer,
+  },
+  middleware: (getDefault) =>
+    getDefault()
+      .concat(apiUser.middleware)
+      .concat(apiOrder.middleware)
+      .concat(apiCustomer.middleware)
+      .concat(apiProduct.middleware)
+      .concat(apiCategory.middleware)
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/scenes/layout/index.jsx
@@ -0,0 +1,40 @@
+import { Box, useMediaQuery } from "@mui/material";
+import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
+
+function Layout() {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const authToken = JSON.parse(JSON.stringify(localStorage.getItem('token')))
+
+  if (!authToken) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        user={{}}
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          user={{}}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+    // : <Navigate to="/login" />
+  );
+}
+
+export default Layout;
